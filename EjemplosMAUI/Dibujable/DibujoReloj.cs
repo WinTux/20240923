75f78c6be255c9f2c8d46860b23b15a1f37a583f
WinTux <rusok.software@gmail.com>
2024-09-23 19:53:24 -0400

    Agregar archivos de proyecto.

---
+++ EjemplosMAUI/Dibujable/DibujoReloj.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjemplosMAUI.Dibujable
+{
+    internal class DibujoReloj : IDrawable
+    {
+        public DibujoReloj()
+        {
+            
+        }
+        public void Draw(ICanvas canvas, RectF dirtyRect)
+        {
+            DateTime tiempoActual = DateTime.Now;
+            var centroReloj = new PointF(200, 300);
+            var radioCirculo = 100;
+
+            canvas.StrokeColor = Colors.Violet;
+            canvas.StrokeSize = 6;
+
+            canvas.DrawCircle(centroReloj, radioCirculo);
+            canvas.DrawCircle(centroReloj, 5);
+
+            canvas.StrokeSize = 4;
+            var horero = GetManecillaHora(tiempoActual, radioCirculo, centroReloj);
+            canvas.DrawLine(centroReloj, horero);
+        }
+    }
+}
